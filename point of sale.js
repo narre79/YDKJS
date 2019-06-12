23be2ee3b98fa7e00570d4d898350e825061464f
normdev <49796937+narre79@users.noreply.github.com>
2019-06-12 16:59:54 -0400

    Update point of sale.js

--- point of sale.js
+++ point of sale.js
@@ -3,15 +3,14 @@ const SalesTax = 0.08;
 const Phone = 99.99;
 const Accessory = 9.99;
 
-let bankAccount = promt("please enter current acount funds:");
+bankAccount = promt("please enter current acount funds:");
 let amount = 0;
 
 function purchaseAmount(amount){
   if (amount >= Threshold) {
     amount = Phone * 2
     return amount;
-    
-}
+  }
 }
 
 function calculateTax(amount){
@@ -21,20 +20,19 @@ function calculateTax(amount){
 function formatAmount(amount){
   return "$" + amount.toFixed(2)
 }
+
 // keep buying phones
 while (amount < bankAccount){
   amount= amount + Phone
-  if (amount < Threshold){
+  
+  if (amount < Threshold)
   amount = amount + Accessory
+}
 
-}
-}
 amount = amount + calculateTax(amount)
 
-console.log("Your purchase: ") + formatAmount(amount)
-};
+console.log("Your purchase: ") + formatAmount(amount);
 
 if(amount > bankAccount){
-  console.log("You can't afford this purchase. :(")
-};
+  console.log("You can't afford this purchase. :(");
 }
